fce80a9da80aec5df132c301a6f9a773b7040735
Prabhas2005 <prabhaschandra2005@gmail.com>
2025-04-17 10:06:30 +0530

    first commit

---
+++ q3.js
@@ -0,0 +1,23 @@
+db.users.insertMany([
+    {
+        name: "Mike",
+        email: "mike@example.com",
+        pass: "1234",
+    },
+    {
+        name: "Cathy",
+        email: "cathy@example.com",
+        pass: "123456",
+    },
+]);
+
+db.users.find();
+
+db.users.findOne(); //used to get the first one
+
+db.users.find().limit(2); //used to get required number
+
+db.users.find().skip(1); //skips the first one
+
+db.users.find().skip(2); //skips the first 2
+
